e52118e938946ddab71eabb9da43b164558a3232
ShehaniWageesha <it19113746@my.sliit.lk>
2020-08-17 11:34:15 +0530

    Intial Commit

---
+++ app/src/main/java/com/example/computersolutions/signupnext.java
@@ -0,0 +1,14 @@
+package com.example.computersolutions;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class signupnext extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signupnext);
+    }
+}
