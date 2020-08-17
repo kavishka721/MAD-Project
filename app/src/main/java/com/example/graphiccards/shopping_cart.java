39c1085126f7c375a56013eadf4d5776f89aa4e6
Wimalki97 <67803441+Wimalki97@users.noreply.github.com>
2020-08-17 11:33:14 +0530

    initial commit

---
+++ app/src/main/java/com/example/graphiccards/shopping_cart.java
@@ -0,0 +1,15 @@
+
+package com.example.graphiccards;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class shopping_cart extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shopping_cart);
+    }
+}
